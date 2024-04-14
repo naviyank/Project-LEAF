952cf2af6b5132cd76a8744584d84a266fa0bd98
naviyank <166953144+naviyank@users.noreply.github.com>
2024-04-15 02:56:35 +0530

    Update Impacket.sh

---
+++ Shell_Scripts/Impacket.sh
@@ -0,0 +1,9 @@
+# Clone the repository
+git clone https://github.com/fortra/impacket.git
+# Change directory to impacket
+cd impacket
+# Install Python3 and pip if not already installed
+sudo apt install python3 python3-pip
+# Install Impacket
+sudo python3 setup.py install
+sudo apt install smbclient
